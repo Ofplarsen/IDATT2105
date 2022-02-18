e098000e46f9436008c8f53552d12277976d6d0c
Olav Finne Præsteng Larsen <ofplarsen@gmail.com>
2022-02-18 16:17:49 +0100

    Initialized oving 4

---
+++ Oving4/Backend/src/main/java/edu/ntnu/idatt2105/backend/TestController.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idatt2105.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test(){
+        return "Hello";
+    }
+}
