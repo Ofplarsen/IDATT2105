8542fc46fe10752f59cc4d3ec6b4b0f3cc2c6dde
ofplarsen <ofplarsen@gmail.com>
2022-03-11 13:30:52 +0100

    Initial commit backend h2

---
+++ Oving5/BackendCrud/src/test/java/edu/ntnu/oflarsen/backendcrud/BackendCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.oflarsen.backendcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
