e4ff7cf469cc3289f2f4836638b638d3b1775f3f
ofplarsen <ofplarsen@gmail.com>
2022-03-11 14:13:00 +0100

    Example with H2 database

--- Oving5/BackendCrud/src/main/java/edu/ntnu/oflarsen/backendcrud/repository/TutorialRepository.java
+++ Oving5/BackendCrud/src/main/java/edu/ntnu/oflarsen/backendcrud/repository/TutorialRepository.java
@@ -1,4 +1,14 @@
 package edu.ntnu.oflarsen.backendcrud.repository;
 
-public class TutorialRepository {
+import java.util.List;
+import edu.ntnu.oflarsen.backendcrud.model.Tutorial;
+public interface TutorialRepository {
+    int save(Tutorial book);
+    int update(Tutorial book);
+    Tutorial findById(Long id);
+    int deleteById(Long id);
+    List<Tutorial> findAll();
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+    int deleteAll();
 }
