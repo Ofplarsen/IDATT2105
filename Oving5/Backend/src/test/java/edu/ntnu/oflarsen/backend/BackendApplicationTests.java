bcb2478e071219f2064d570745ceab42abde3908
ofplarsen <ofplarsen@gmail.com>
2022-03-11 12:55:20 +0100

    Initial commit backend

---
+++ Oving5/Backend/src/test/java/edu/ntnu/oflarsen/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.oflarsen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
