69ecb029b5e1710077bae2a22b47ffc5e5f6d91e
ofplarsen <ofplarsen@gmail.com>
2022-03-12 13:37:28 +0100

    example of backend with jpa

---
+++ Oving5/BackendJPA/src/main/java/edu/ntnu/oflarsen/backendjpa/repository/TutorialRepository.java
@@ -0,0 +1,9 @@
+package edu.ntnu.oflarsen.backendjpa.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import edu.ntnu.oflarsen.backendjpa.model.Tutorial;
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
